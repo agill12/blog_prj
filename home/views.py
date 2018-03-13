3fbacde81c86a08a0049570937a34c56884af6d7
Anne Gill <gill01730@gmail.com>
2018-03-13 14:41:48 +0000

    Created accounts and home views and urls

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from messenger.models import Message
 
 # Create your views here.
+def get_index(request):
+    
+    return render(request, "home/post_list.html")
